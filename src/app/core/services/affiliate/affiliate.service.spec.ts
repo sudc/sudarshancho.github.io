2899a195e243179b3aecb681959b8ae4f35dd0ac
Sudarshan C <Sudarshan.C@ril.com>
2025-12-25 12:44:23 +0530

    Simplify CORS - allow all origins for debugging

--- src/app/core/services/affiliate/affiliate.service.spec.ts
+++ src/app/core/services/affiliate/affiliate.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 import { AffiliateService } from './affiliate.service';
 import { AffiliateConfigService, AffiliateConfigData } from '../affiliate-config.service';
 
